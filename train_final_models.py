e00067ec0f0d21c1a4624f60a081098dd74122a0
kjaa2018 <kjaa2018@POM-ITB-DGX01.campus.pomona.edu>
2022-05-05 14:51:14 -0700

    tuning is working but final run function causes crash due to vector sizes

---
+++ train_final_models.py
@@ -0,0 +1,20 @@
+import sys
+import numpy as np
+from matplotlib import pyplot
+import torch
+import torch.nn as nn
+from torch import optim
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
+
+# from icecream import ic
+import pandas as pd
+from math import sqrt, inf
+import glob
+import csv
+
+from pathlib import Path
+import json
+
+
+import random
